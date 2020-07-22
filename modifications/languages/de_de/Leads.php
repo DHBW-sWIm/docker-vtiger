87df3f9a9f97f70cebc196b3e3736e36a964960e
Ethan Kelly <kelly.ethan99@gmail.com>
2020-07-22 11:49:52 +0200

    Merge changes

---
+++ modifications/languages/de_de/Leads.php
@@ -0,0 +1,59 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	'Leads'                        => 'Potenzielle Studenten'                       , 
+	'SINGLE_Leads'                 => 'Potenzieller Studenten'                        , 
+	'LBL_RECORDS_LIST'             => 'Leads List'                  , 
+	'LBL_ADD_RECORD'               => 'Potenziellen Student hinzufügen'                    , 
+	'LBL_LEAD_INFORMATION'         => 'Potenzieller Student'                        , 
+	'Lead No'                      => 'Lead Nr.'                    , 
+	'Company'                      => 'Organisation'                , 
+	'Designation'                  => 'Funktion'                    , 
+	'Website'                      => 'Webseite'                    , 
+	'Industry'                     => 'Branche'                     , 
+	'Lead Status'                  => 'Leadstatus'                  , 
+	'No Of Employees'              => 'Anzahl Mitarbeiter'          , 
+	'--None--'                     => '--ohne--'                    , 
+	'Mr.'                          => 'Sehr geehrter Herr'          , 
+	'Ms.'                          => 'Sehr geehrte Frau'           , 
+	'Mrs.'                         => 'Sehr geehrte Frau'           , 
+	'Dr.'                          => 'Sehr geehrter Herr Dr.'      , 
+	'Prof.'                        => 'Sehr geehrter Herr Prof.'    , 
+	'Attempted to Contact'         => 'Kontaktierung versucht'      , 
+	'Cold'                         => 'kalt'                        , 
+	'Contact in Future'            => 'in der Zukunft kontaktieren' , 
+	'Contacted'                    => 'kontaktiert'                 , 
+	'Hot'                          => 'heiss'                       , 
+	'Junk Lead'                    => 'wertloser Lead'              , 
+	'Lost Lead'                    => 'Lead verloren'               , 
+	'Not Contacted'                => 'Nicht kontaktiert'           , 
+	'Pre Qualified'                => 'vorqualifiziert'             , 
+	'Qualified'                    => 'qualifiziert'                , 
+	'Warm'                         => 'warm'                        , 
+	'LBL_CONVERT_LEAD'             => 'Lead umwandeln:'             , 
+	'LBL_TRANSFER_RELATED_RECORD'  => 'Transfer related record to'  , 
+	'LBL_CONVERT_LEAD_ERROR'       => 'You have to enable either Organization or Contact to convert the Lead', 
+	'LBL_LEADS_FIELD_MAPPING_INCOMPLETE' => 'Alle Pflichtfelder sind nicht zugeordnet', 
+	'LBL_LEADS_FIELD_MAPPING'      => 'Zuordnung der benutzerdefinierten Lead Felder', 
+	'LBL_CUSTOM_FIELD_MAPPING'     => 'Lead-Konvertierung Datenzuordnung'          , 
+    'LBL_IMAGE_INFORMATION' => 'Profil Bild',
+    'Lead Image' => 'Lead Bild',
+	
+
+  'Phone' => 'Primäre Telefon',
+  'Secondary Email' => 'Sekundäre E-Mail',
+  'Email' => 'Primäre E-Mail',
+
+);
+$jsLanguageStrings = array(
+	'JS_SELECT_CONTACTS'           => 'Select Contacts to proceed'  , 
+	'JS_SELECT_ORGANIZATION'       => 'Select Organization to proceed', 
+	'JS_SELECT_ORGANIZATION_OR_CONTACT_TO_CONVERT_LEAD' => 'Conversion requires selection of Contact or Organization', 
+);
